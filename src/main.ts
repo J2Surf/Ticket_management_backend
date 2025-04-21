0ae423875a7b81aab129e6e2d32bf9b8a7000ff9
Jerin <jerin.dev.jobi@gmail.com>
2025-04-22 00:10:25 +0900

    fix: cors to all

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:5173'], // Frontend URL
+    origin: '*', // Frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
